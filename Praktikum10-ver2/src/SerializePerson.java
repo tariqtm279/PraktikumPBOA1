db00b4004b33634718bb279b04e5e7637ef78995
tariqtm279 <munawararsy@gmail.com>
2026-05-16 22:14:24 +0700

    Add files via upload

---
+++ Praktikum10-ver2/src/SerializePerson.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author arsym
+ */
+import java.io.*;
+
+class Person implements Serializable {
+    private String name;
+
+    public Person(String n) {
+        name = n;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+public class SerializePerson {
+    public static void main(String[] args) {
+        Person person = new Person("Panji");
+
+        try {
+            FileOutputStream f = new FileOutputStream("person.ser");
+            ObjectOutputStream s = new ObjectOutputStream(f);
+
+            s.writeObject(person);
+
+            System.out.println("Selesai menulis objek person");
+
+            s.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+    
